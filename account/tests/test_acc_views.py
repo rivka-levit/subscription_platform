d902ba16f7095e0ce90360c38a6ea3c78531e006
rivka-yuliia <juliacreadora@gmail.com>
2024-11-26 20:00:03 +0200

    test has form and csrf

--- account/tests/test_acc_views.py
+++ account/tests/test_acc_views.py
@@ -1,6 +1,9 @@
 import pytest
 from django.urls import reverse
 
+from account.forms import CreateUserForm
+
+
 @pytest.mark.parametrize(
     'url_name, expected',
     [('', 200), ('register', 200), ('login', 200)]
@@ -9,3 +12,22 @@ def test_get_page_success(url_name, expected, client):
     r = client.get(reverse(url_name))
 
     assert r.status_code == expected
+
+
+@pytest.mark.django_db
+def test_register_get_page_has_form(client):
+    """Test register page contains form and it is CreateUserForm instance."""
+
+    r = client.get(reverse('register'))
+    form = r.context['register_form']
+
+    assert 'register_form' in r.context
+    assert isinstance(form, CreateUserForm)
+
+
+@pytest.mark.django_db
+def test_register_get_page_csrf(client):
+    """Test register page contains csrf token."""
+
+    r = client.get(reverse('register'))
+    assert 'csrf_token' in r.context
