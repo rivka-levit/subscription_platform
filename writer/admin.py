2ae75642be0b4eb8ac902a73fe8d06a83a02f08f
rivka-yuliia <juliacreadora@gmail.com>
2024-12-01 05:11:02 +0200

    article model and admin

--- writer/admin.py
+++ writer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from writer.models import Article
+
+@admin.register(Article)
+class ModelNameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted', 'last_modified', 'is_premium')
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ('is_premium',)
--- writer/models.py
+++ writer/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField(max_length=10000, blank=True)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    date_posted = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_premium = models.BooleanField(
+        default=False,
+        verbose_name="Is it a premium article?"
+    )
+
+    class Meta:
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
