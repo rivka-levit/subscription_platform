13f81b0e18e099df0c7a0d6431cc076987f4fe13
rivka-yuliia <juliacreadora@gmail.com>
2024-12-17 06:33:02 +0200

    Subscription model

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from client.models import Subscription
+
+
+admin.site.register(Subscription)
--- client/models.py
+++ client/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class Subscription(models.Model):
+    STANDARD = 'STANDARD'
+    PREMIUM = 'PREMIUM'
+
+    PAYPAL_PLAN_CHOICES = {
+        STANDARD: 'Standard',
+        PREMIUM: 'Premium'
+    }
+
+    subscriber_name = models.CharField(max_length=255)
+    subscription_plan = models.CharField(
+        max_length=255,
+        choices=PAYPAL_PLAN_CHOICES,
+        default=STANDARD
+    )
+    subscription_cost = models.DecimalField(decimal_places=2, max_digits=8)
+    paypal_subscription_id = models.CharField(max_length=300)
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        max_length=10,
+        unique=True
+    )
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.subscriber_name} - {self.subscription_plan}'
