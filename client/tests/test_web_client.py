94401c9d346e1a10f3efe2b1b295457f67d36a75
rivka-yuliia <juliacreadora@gmail.com>
2024-12-21 22:08:40 +0200

    test browse article page

--- client/tests/test_web_client.py
+++ client/tests/test_web_client.py
@@ -40,3 +40,43 @@ def test_get_dashboard_writer_redirect_to_writer_dashboard(client, user_writer):
     assert r.status_code == 302
     assert r['Location'] == reverse('writer:dashboard',
                                     kwargs={'writer_id': user_writer.id})
+
+
+def test_browse_articles_without_subscription_empty(client, sample_user):
+    """Test browse articles page without subscription does not provide
+    articles."""
+
+    client.force_login(sample_user)
+    r = client.get(reverse('client:browse-articles'))
+
+    assert r.status_code == 200
+    assert 'articles' in r.context
+    assert 'no active subscription' in r.content.decode('utf-8')
+
+
+@pytest.mark.parametrize('plan,expected_count',
+                         [('STANDARD', 2), ('PREMIUM', 3)])
+def test_browse_articles_with_subscription(
+        plan,
+        expected_count,
+        client,
+        sample_user,
+        user_writer,
+        subscription,
+        article
+):
+    """Test browse articles page with subscription retrieves correct number
+    of articles."""
+
+    article(user_writer, title='Article 1')
+    article(user_writer, title='Article 2')
+    article(user_writer, title='Article 3', is_premium=True)
+
+    subscription(user=sample_user, subscription_plan=plan)
+    client.force_login(sample_user)
+
+    r = client.get(reverse('client:browse-articles'))
+
+    assert r.status_code == 200
+    assert 'articles' in r.context
+    assert len(r.context['articles']) == expected_count
